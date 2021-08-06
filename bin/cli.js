63a381172795a0d65900044801e150b570806694
Artem Rudenko <rtomrud@gmail.com>
2021-08-06 02:37:33 +0200

    Refactor to use async/await

--- bin/cli.js
+++ bin/cli.js
@@ -42,21 +42,22 @@ and the title tag "Ode to Joy" is renamed to "beethoven-ode-to-joy.mp3".`);
     tags: Array.isArray(opts.t) ? opts.t : [opts.t],
   };
   let jobs = paths.length;
-  const work = (path) =>
-    tagtoname(path, options)
-      .then((dest) => console.log(dest))
-      .catch(({ message }) => {
-        console.error(`${path}: ${message}`);
-        process.exitCode = 1;
-      })
-      .then(() => {
-        if (jobs === 0) {
-          return null;
-        }
+  const work = async (path) => {
+    try {
+      const dest = await tagtoname(path, options);
+      console.log(dest);
+    } catch ({ message }) {
+      process.exitCode = 1;
+      console.error(`${path}: ${message}`);
+    }
 
-        jobs -= 1;
-        return work(paths[jobs]);
-      });
+    if (jobs === 0) {
+      return null;
+    }
+
+    jobs -= 1;
+    return work(paths[jobs]);
+  };
 
   for (let workers = 0; jobs > 0 && workers < 4; workers += 1) {
     jobs -= 1;
--- index.js
+++ index.js
@@ -4,24 +4,20 @@ const {
 } = require("fs");
 const { basename, dirname, extname, join } = require("path");
 const { promisify } = require("util");
-const { lock, unlock } = require("lockfile");
+const { lock: lockCb, unlock: unlockCb } = require("lockfile");
 const { parseFile } = require("music-metadata");
 const slugify = require("standard-slugify");
 
-const lockPromise = promisify(lock);
-const unlockPromise = promisify(unlock);
+const lock = promisify(lockCb);
+const unlock = promisify(unlockCb);
 
-const renameWithLock = (oldPath, newPath) => {
-  const dir = dirname(newPath);
-  const lockFile = join(dir, `${basename(newPath)}.lock`);
-  return lockPromise(lockFile)
-    .catch((error) =>
-      error.code === "ENOENT"
-        ? mkdir(dir, { recursive: true })
-        : Promise.reject(error)
-    )
-    .then(() => rename(oldPath, newPath))
-    .then(() => unlockPromise(lockFile));
+const exists = async (path) => {
+  try {
+    await access(path, constants.F_OK);
+    return true;
+  } catch {
+    return false;
+  }
 };
 
 /**
@@ -40,7 +36,7 @@ const renameWithLock = (oldPath, newPath) => {
  * - `tags`: An array of the tags used in the new name,
  * defaults to `["artist", "title"]`
  */
-module.exports = function (
+module.exports = async function (
   path = "",
   {
     keepCase = false,
@@ -49,33 +45,49 @@ module.exports = function (
     tags = ["artist", "title"],
   } = {}
 ) {
-  return parseFile(path).then(({ common }) => {
-    const name = tags
-      .reduce((tags, key) => {
-        const value = common[key];
-        const tag = typeof value === "object" ? Object.values(value)[0] : value;
-        if (tag != null) {
-          tags.push(slugify(String(tag), { keepCase }));
-        }
+  const { common } = await parseFile(path);
+  const name = tags
+    .reduce((tags, key) => {
+      const value = common[key];
+      const tag = typeof value === "object" ? Object.values(value)[0] : value;
+      if (tag != null) {
+        tags.push(slugify(String(tag), { keepCase }));
+      }
 
-        return tags;
-      }, [])
-      .join(separator);
+      return tags;
+    }, [])
+    .join(separator);
+  if (name === "") {
+    throw Error(`Failed because '${path}' is missing all tags`);
+  }
 
-    if (name === "") {
-      return Promise.reject(
-        Error(`Failed because '${path}' is missing all tags`)
-      );
+  const newPath = join(dirname(path), name + extname(path));
+  if (path === newPath) {
+    return newPath;
+  }
+
+  if (await exists(newPath)) {
+    throw Error(`Failed because '${newPath}' already exists`);
+  }
+
+  if (noop) {
+    return newPath;
+  }
+
+  const dir = dirname(newPath);
+  const lockFile = join(dir, `${basename(newPath)}.lock`);
+  try {
+    await lock(lockFile);
+  } catch (error) {
+    if (error.code !== "ENOENT") {
+      throw error;
     }
 
-    const newPath = join(dirname(path), name + extname(path));
-    return path === newPath
-      ? newPath
-      : access(newPath, constants.F_OK).then(
-          () =>
-            Promise.reject(Error(`Failed because '${newPath}' already exists`)),
-          () =>
-            noop ? newPath : renameWithLock(path, newPath).then(() => newPath)
-        );
-  });
+    await mkdir(dir, { recursive: true });
+    await lock(lockFile);
+  }
+
+  await rename(path, newPath);
+  await unlock(lockFile);
+  return newPath;
 };
